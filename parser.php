27275dd80418a6b9bc87a83578f701d64ce85e62
Danil <makarovdanil289@gmail.com>
2023-03-09 17:02:34 +0300

    added parser films and pulled bd

--- parser.php
+++ parser.php
@@ -24,13 +24,35 @@
     $xml = simplexml_load_file("xml/movies.xml") or die("Error: Cannot create odject");
 
     // echo count($xml);
+    // $title = null;
+    // $title_orign = null;
+    // $post = null;
+    // $rating = null;
+    // $year = null;
 
-    foreach ($xml as $movie_key => $movie) {
-        echo $movie->title_russian."<br>";
-    }
+    // foreach ($xml as $movie_key => $movie) {
+    //     $title = $movie->title_russian;
+    //     $title_orign = $movie->title_original;
+    //     $year = $movie->year;
+         
+
+    //     foreach ($movie->poster->big->attributes() as $poster_key => $poster) {
+    //         $post = $poster;
+    //     }
+
+    //     if($movie->imdb) {
+    //         $rating = $movie->imdb->attributes()['rating'];
+    //     } else {
+    //         $rating = null;
+    //     }
+    //     insert($title, $title_orign, $year, $rating, $post, 1);
+    // }
+
+    $xmlserials = simplexml_load_file("xml/serials.xml") or die("Error: Cannot create object");
+
 
     echo "<pre>";
-    print_r($xml);
+    print_r($xmlserials);
     echo "</pre>";
 
 
